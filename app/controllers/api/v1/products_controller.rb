eb3c068d3733be977e4e8708367fe221f403fc56
Aldo Witzke <aldo_mwa@hotmail.com>
2019-06-27 16:50:31 -0300

    product API CRUD

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ProductsController < Api::V1::ApplicationController
+  before_action :set_product, only: [ :show, :update, :destroy ]
+  skip_before_action :verify_authenticity_token
+  
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+  
+  def update
+    if @product.update(product_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
+
+  def set_product
+    @product = Product.find(params[:id]) 
+  end
+
+  def render_error
+    render json: { errors: @product.errors.full_messages },
+      status: :unprocessable_entity
+  end
+end
