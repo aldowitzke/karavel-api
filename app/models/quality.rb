1674efb4922665128e99fd8b2e87bc3ec3873f36
Aldo Witzke <aldo_mwa@hotmail.com>
2019-07-01 16:04:46 -0300

    dependent destroy species and quality for export standards

--- app/models/quality.rb
+++ app/models/quality.rb
@@ -1,5 +1,6 @@
 class Quality < ApplicationRecord
   has_many :species, through: :exportation_standards
+  has_many :exportation_standards, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -1,5 +1,6 @@
 class Species < ApplicationRecord
   has_many :qualities, through: :exportation_standards
+  has_many :exportation_standards, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
